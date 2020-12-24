99ef9b24429a8d6ccb786f3c3047ed840cf68d83
shreyas333 <55734579+shreyas333@users.noreply.github.com>
2020-12-24 19:11:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,19 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-a1 = html.Div(["Input: ",
-              dcc.Input(id='my-input', value='initial value', type='text')])
-
 app.layout = html.Div([
     html.H6("Change the value in the text box to see callbacks in action!"),
-    a1,
+    html.Div(["Input: ",
+              dcc.Input(id='my-input', value='initial value', type='text')]),
     html.Br(),
     html.Div(id='my-output'),
 
 ])
 
-b1 = Input(component_id='my-input', component_property='value')
+
 @app.callback(
     Output(component_id='my-output', component_property='children'),
-    b1
+    Input(component_id='my-input', component_property='value')
 )
 def update_output_div(input_value):
     return 'Output: {}'.format(input_value)
